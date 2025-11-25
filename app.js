d818d390c95ec5084064b3812c9e499311a0c9ec
Vitaliy <vitaliy.yashnikov@gmail.com>
2025-11-25 21:22:50 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer(function (req, res) {
+    let pathNameFull = req.url;
+    pathNameObj = url.parse(pathNameFull, true);
+    pathName = pathNameObj.pathname;
+
+    if (pathName === '/') {
+        const htmlPath = path.join(__dirname, 'templates', 'page.html');
+        const fileStream = fs.createReadStream(htmlPath, 'UTF-8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fileStream.pipe(res);
+
+
+    } else if (req.url.match('[.]html$')) {
+        const htmlPath = path.join(__dirname, 'templates', req.url);
+        const fileStream = fs.createReadStream(htmlPath, 'UTF-8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fileStream.pipe(res);
+
+    } else if (req.url.match('[.]css$')) {
+        const cssPath = path.join(__dirname, 'templates', req.url);
+        const fileStream = fs.createReadStream(cssPath, 'UTF-8');
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        fileStream.pipe(res);
+
+    
+    } else if (req.url.match('[.]png$' )) {
+    const imgPath = path.join(__dirname, '', req.url);
+    console.log(imgPath)
+    const fileStream = fs.createReadStream(imgPath);
+    res.writeHead(200, { 'Content-Type': 'image/png' });
+    fileStream.pipe(res);
+
+}
+    else if (req.url.match('[.]jpeg$')) {
+        const imgPath = path.join(__dirname, '', req.url);
+        console.log(imgPath)
+        const fileStream = fs.createReadStream(imgPath);
+        res.writeHead(200, { 'Content-Type': 'image/jpg' });
+        fileStream.pipe(res);
+
+    } else {
+    res.writeHead(404, { 'Content-Type': 'text/html', });
+    res.end('<h1>Page not found</h1>');
+}
+    });
+
+server.listen(3000); //listen for any incoming requests
+console.log('Node.js web server at port 3000 is running..');
